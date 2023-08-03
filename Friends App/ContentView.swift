78b630b78fe4eae51d7a958d4f270ce7e26b6ca2
Mohamed Badir <glury6@gmail.com>
2023-08-03 16:28:15 +0400

    Initial Commit

---
+++ Friends App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Friends App
+//
+//  Created by Mohamed Badir on 03/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friends App/Friends_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friends_AppApp.swift
+//  Friends App
+//
+//  Created by Mohamed Badir on 03/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Friends_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
