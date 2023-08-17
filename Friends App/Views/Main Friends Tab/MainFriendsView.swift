dd201dd557c66f1b18e529d635e9916612d2b9ba
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-08-17 14:39:13 +0400

    Added sorting to the friend app, placed under the 3 dots menu, you can sort by "Name" or if they are best friend or not (best friends come first)

--- Friends App/Views/Main Friends Tab/MainFriendsView.swift
+++ Friends App/Views/Main Friends Tab/MainFriendsView.swift
@@ -4,40 +4,64 @@ struct MainFriendListView: View {
     @State private var showAddSheet = false
     @State private var showConfirmAlert = false
     @ObservedObject var friendManager: FriendManager
-
+    
     var body: some View {
-        NavigationStack {
-            List($friendManager.friends, editActions: [.all]) { $friend in
-                FriendRowView(friend: $friend)
-            }
-            .searchable(text: $friendManager.serchTerms)
-            .navigationTitle("Friends")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    EditButton()
-                }
-                ToolbarItemGroup(placement: .navigationBarTrailing){
-                    Button{
-                        showConfirmAlert = true
-                    } label: {
-                        Image(systemName: "list.bullet.clipboard.fill")
+        ZStack {
+            VStack {
+                NavigationStack {
+                    List($friendManager.friends, editActions: [.all]) { $friend in
+                        FriendRowView(friend: $friend)
                     }
-                    Button{
-                        showAddSheet = true
-                    } label: {
-                        Image(systemName: "plus")
+                    .searchable(text: $friendManager.serchTerms)
+                    .navigationTitle("Friends")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            EditButton()
+                        }
+                        ToolbarItemGroup(placement: .navigationBarTrailing){
+                            Button {
+                                showConfirmAlert = true
+                            } label: {
+                                Image(systemName: "list.bullet.clipboard.fill")
+                            }
+                            
+                            #if DEBUG
+                            Button {
+                                showAddSheet = true
+                            } label: {
+                                Image(systemName: "plus")
+                            }
+                            #endif
+                            
+                            Menu {
+                                Text("Sort By...")
+                                Button("Name", action: sortByName)
+                                Button("Preference", action: sortByBest)
+                                
+                            } label: {
+                                Image(systemName: "ellipsis.circle")
+                            }
+                        }
                     }
-                }
-            }
-            .sheet(isPresented: $showAddSheet){
-                NewFriendView(sourceArray: $friendManager.friends)
-            }
-            .alert("Load sample data? Warning: This cannot be undone!", isPresented: $showConfirmAlert) {
-                Button("Replace", role: .destructive) {
-                    friendManager.loadSampleData()
+                    .sheet(isPresented: $showAddSheet){
+                        NewFriendView(sourceArray: $friendManager.friends)
+                    }
+                    .alert("Load sample data? Warning: This cannot be undone!", isPresented: $showConfirmAlert) {
+                        Button("Replace", role: .destructive) {
+                            friendManager.loadSampleData()
+                        }
+                    }
                 }
             }
         }
+    }
+    
+    func sortByName() {
+        friendManager.friends.sort { $0.name.lowercased() <  $1.name.lowercased() }
+    }
+    
+    func sortByBest() {
+        friendManager.friends.sort { ($0.isBestFriend ? 0 : 1 ) < ($1.isBestFriend ? 0 : 1 ) }
     }
 }
 
